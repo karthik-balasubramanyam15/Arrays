4f2e1ba30abe4d81e1cf79183ea40661d64b79bb
Karthik <kbalasub@stevens.edu>
2021-04-17 20:42:10 -0500

    updated Reverse String with other ways

--- ReverseString.js
+++ ReverseString.js
@@ -9,5 +9,14 @@ function reverse(str) {
     return reverseCharArray.join('');
 }
 
+function reverse2(str) {
+    if (!str || str.length < 2 || typeof str !== 'string') return str;
+    return str.split('').reverse().join('');
+}
+
+const reverse3 = str => [...str].reverse().join('');
+
 console.log(reverse('Karr is Awesome!'));
+console.log(reverse2('Awesome!'));
+console.log(reverse3('Aweee'));
 console.log(reverse(1234));
