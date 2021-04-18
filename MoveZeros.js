9b4b821a67d0c88cf3a719f48c4b6f6df80bf030
Karthik <kbalasub@stevens.edu>
2021-04-18 05:19:02 -0500

    Move zeros

---
+++ MoveZeros.js
@@ -0,0 +1,21 @@
+var moveZeroes = function (nums) {
+
+    let count = 0;
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] !== 0) {
+            nums[count] = nums[i];
+            count++;
+        }
+    }
+
+    while (count < nums.length) {
+        nums[count] = 0;
+        count++;
+    }
+
+
+
+    return nums;
+};
+
+console.log(moveZeroes([0, 1, 0, 3, 12]))
